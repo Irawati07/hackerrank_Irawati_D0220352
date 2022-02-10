a1a7a4c61cf3d31f2418316157cd3e08307ec037
Irawati <irawatiira353@gmail.com>
2022-02-11 06:45:57 +0700

    thirteen commit

---
+++ day10_java_string_introduction.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int LA = A.length();
+        int LB = B.length();
+        int L = LA + LB;
+        System.out.println(L);
+        
+        if(A.compareTo(B)>0){
+            System.out.println("Yes");  
+        }else{
+            System.out.println("No");
+        }
+        
+        String newA1 = A.substring(0,1).toUpperCase();
+        String newA2 = A.substring(1);
+        String newA = newA1 + newA2;
+        
+        String newB1 = B.substring(0,1).toUpperCase();
+        String newB2 = B.substring(1);
+        String newB = newB1 + newB2;
+        System.out.println(newA+" "+newB);
+    }
+}
